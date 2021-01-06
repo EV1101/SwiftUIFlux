e60d6de1ff06a005c72d911f0dfe21765f623055
Ugo Arangino <ugo@arangino.de>
2021-01-06 03:06:35 +0100

    Allow to set custom DispatchQueue

--- Sources/SwiftUIFlux/Store.swift
+++ Sources/SwiftUIFlux/Store.swift
@@ -6,43 +6,56 @@
 //  Copyright © 2019 Thomas Ricouard. All rights reserved.
 //
 
+import Combine
 import Foundation
 import SwiftUI
-import Combine
 
-final public class Store<StoreState: FluxState>: ObservableObject {
+public final class Store<StoreState: FluxState>: ObservableObject {
     @Published public var state: StoreState
 
     private var dispatchFunction: DispatchFunction!
     private let reducer: Reducer<StoreState>
-    
-    public init(reducer: @escaping Reducer<StoreState>,
-                middleware: [Middleware<StoreState>] = [],
-                state: StoreState) {
+    private let dispatchQueue: DispatchQueue
+
+    public init(
+        reducer: @escaping Reducer<StoreState>,
+        middleware: [Middleware<StoreState>] = [],
+        state: StoreState,
+        dispatchQueue: DispatchQueue = .main
+    ) {
         self.reducer = reducer
         self.state = state
-        
+        self.dispatchQueue = dispatchQueue
+
         var middleware = middleware
         middleware.append(asyncActionsMiddleware)
-        self.dispatchFunction = middleware
+        dispatchFunction = middleware
             .reversed()
             .reduce(
                 { [unowned self] action in
-                    self._dispatch(action: action) },
+                    self._dispatchOnMainQueue(action: action)
+                },
                 { dispatchFunction, middleware in
                     let dispatch: (Action) -> Void = { [weak self] in self?.dispatch(action: $0) }
                     let getState = { [weak self] in self?.state }
                     return middleware(dispatch, getState)(dispatchFunction)
-            })
+                }
+            )
     }
 
     public func dispatch(action: Action) {
-        DispatchQueue.main.async {
+        dispatchQueue.async {
             self.dispatchFunction(action)
         }
     }
-    
+
     private func _dispatch(action: Action) {
         state = reducer(state, action)
+    }
+
+    private func _dispatchOnMainQueue(action: Action) {
+        DispatchQueue.main.async {
+            self._dispatch(action: action)
+        }
     }
 }
